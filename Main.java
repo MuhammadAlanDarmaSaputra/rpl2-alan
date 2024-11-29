197f91fc1a579b18828a01b98d7c1f3775195ed1
MuhammadAlanDarmaSaputra <alandarsz20@gmail.com>
2024-11-29 09:13:34 +0700

    ubah file Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String[] args){
-      System.out.println(x:"Belajar Git");  
+      System.out.println(x:"Belajar Git"); 
+      System.out.println(x:"Halooo");
     }
 }
